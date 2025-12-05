0fc367265f1dc1815fd8b8d238a617a6dfade3e7
intan258717-cell <intan258717@student.smkn1kandeman.sch.id>
2025-12-05 13:12:49 +0700

    Add files via upload

---
+++ PRALTIK 5.8-INTAN-XPPLG1/PRALTIK 5.8-INTAN-XPPLG1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRALTIK_5._8_INTAN_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                // --- Blok Proses yang akan diulang ---
+                Console.WriteLine("==============================");
+                Console.WriteLine("Masukkan nama Anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine("Halo, {nama}! Proses telah selesai.")
+                // -------------------------------
+                // Pertanyaan untuk Mengulang 
+                Console.Write("Apakah Anda ingin mengulang lagi? (ya/tidak): ");
+                ulangPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapiaan
+
+                // Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+            } while (ulangPilihan == "ya");
+            Console.WriteLine("Terima kasih. Program selesai.");
+            }
+        }
+    }
+}
